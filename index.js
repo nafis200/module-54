de1589f3271a1e886f1295505520c8ae9373ce62
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-18 22:03:17 +0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5003;
-
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
 const users = [
      {id: 1, name: 'sabana',email:'sabana@gmail.com'},
      {id: 2, name: 'sabnur',email:'sabanur@gmail.com'},
@@ -14,6 +16,15 @@ app.get('/', (req, res) => {
 
 app.get('/users',(req,res)=>{
     res.send(users)
+})
+
+app.post('/users',(req,res)=>{
+    console.log('post api hittings');
+    console.log(req.body);
+    const newUser = req.body 
+    newUser.id = users.length + 1
+    users.push(newUser)
+    res.send(newUser)
 })
 
 app.listen(port, () => {
